67474f198d6a83244dd22f7fe55d1069cd54fc2c
Patrik Ž <patrikzalman@gmail.com>
2022-06-18 22:45:21 +0200

    PISOMKAoop

---
+++ PISOMKAoop/pisomkaZaver/Classa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pisomkaZaver
+{
+    internal class Classa
+    {
+        string ucitel;
+        string meno;
+        string trieda;
+        string projekt;
+        public Classa(string ucitel, string meno, string trieda, string projekt)
+        {
+            this.ucitel = ucitel;
+            this.meno = meno;
+            this.trieda = trieda;
+            this.projekt = projekt;
+        }
+        public Classa(string ucitel, string projekt)
+        {
+            this.ucitel = ucitel;
+            this.projekt = projekt;
+        }
+        public string Ucitel
+        {
+            get
+            {
+                return ucitel;
+            } 
+        }
+        public string Meno
+        {
+            get
+            {
+                return meno;
+            } 
+        }
+        public string Tieda
+        {
+            get
+            {
+                return trieda;
+            } 
+        }
+        public string Projekt
+        {
+            get
+            {
+                return projekt;
+            } 
+        }
+    }
+}
---
+++ PISOMKAoop/pisomkaZaver/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pisomkaZaver
+{
+    public partial class Form1 : Form
+    {
+        List<Classa> supiska = new List<Classa>
+        {
+            new Classa("ko", "nina", "4ag", "photoshop"),
+            new Classa("ko", "tana", "4ag", "powerpoint"),
+            new Classa("re", "lara", "4at", "unity"),
+            new Classa("pi", "kaja", "4ai", "fusion360"),
+            new Classa("pa", "sara", "4bi", "cisco")
+        };
+
+        List<TextBox> filterBoxy;
+
+        public Form1()
+        {
+            InitializeComponent();
+            filterBoxy = new List<TextBox>{textBox1, textBox2, textBox3, textBox4};
+            vypis();
+        }
+
+        private void vypis()
+        {
+            richTextBox1.Clear();
+            foreach (var item in supiska)
+            {
+                if (fiterCheck(item))
+                {
+                    richTextBox1.AppendText(item.Ucitel + "  " + item.Meno + "  " + item.Tieda + "  " + item.Projekt + "\n");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vypis();
+        }
+        private bool fiterCheck(Classa item)
+        {
+            string[] itemy = { item.Ucitel, item.Meno, item.Tieda, item.Projekt};
+            for (int i = 0; i < 4; i++)
+            {
+                if(!(filterBoxy[i].Text == "" || itemy[i].ToLower().Contains(filterBoxy[i].Text.ToLower())))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
